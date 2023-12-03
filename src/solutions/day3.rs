62d0206125d22d86f894bb749cc006b34b7f058f
martin <martin.kurtz@icloud.com>
2023-12-03 21:22:11 +0100

    part 2 done

--- src/solutions/day3.rs
+++ src/solutions/day3.rs
@@ -5,11 +5,11 @@ use super::super::read_file;
 pub fn run() {
     // read file to string
     let input = read_file(3).expect("Couldn't read file");
-    let map = Map::new(input.clone());
+    let map = Map::new(input);
 
-    println!("Day 3, part 1 normal: {}", map.part_sum());
+    println!("Day 3, part 1 {}", map.part_sum());
+    println!("Day 3, part 2 {}", map.sum_gear_ratio());
 
-    // println!("Day 3, part 1 - {}", todo!());
 }
 
 #[derive(Debug)]
@@ -20,22 +20,32 @@ struct Number {
     end: usize,
 }
 
+#[derive(Debug)]
+struct PotentialGear {
+    row: usize,
+    position: usize,
+}
+
 struct Map {
     map: String,
     numbers: Vec<Number>,
+    potential_gears: Vec<PotentialGear>,
 }
 
 impl Map {
     fn new(input: String) -> Self {
         let numbers = vec![];
+        let potential_gears = vec![];
         let mut map = Map {
             map: input,
             numbers,
+            potential_gears,
         };
 
         // calculations:
         map.find_numbers();
         map.part_sum();
+        map.find_potential_gears();
 
         map
     }
@@ -87,8 +97,9 @@ impl Map {
         let mut line_iter = self.map.lines().skip(skip_rows).take(take_lines);
 
         while let Some(line) = line_iter.next() {
-            let mut char_left_right = line.chars().skip(skip_characters).take(take_characters);
-            while let Some(c) = char_left_right.next() {
+            let mut surrounding_characters = line.chars().skip(skip_characters).take(take_characters);
+            
+            while let Some(c) = surrounding_characters.next() {
                 if !c.is_digit(10) && c != '.' {
                     return true;
                 }
@@ -109,6 +120,77 @@ impl Map {
 
         sum
     }
+
+    fn find_potential_gears(&mut self) {
+        let re = Regex::new(r"(\*)").unwrap();
+        
+        for (row, line) in self.map.lines().enumerate() {
+            for captures in re.captures_iter(line).map(|c| c) {
+                if let Some(capture) = captures.iter().next() {
+                    if let Some(capture) = capture {
+                        let position = capture.start();
+                        let end = capture.end();
+                        self.potential_gears.push(PotentialGear { 
+                            row,
+                            position,
+                
+                        });
+                    }
+                }
+            }
+        }
+        
+    }
+
+    fn is_gear(&self, gear: &PotentialGear) -> Option<Vec<&Number>> {
+        let mut connected_gears = vec![];
+        
+        for number in &self.numbers {
+            
+            if (gear.row > 0 && gear.row - 1 == number.row) 
+                || gear.row == number.row
+                || gear.row + 1 == number.row {
+
+                    let start = if number.start == 0 {
+                        0
+                    } else {
+                       number.start -1
+                    };
+
+                    let end = number.end + 1;
+
+                    let range = start..end;
+
+                    if range.contains(&gear.position)  {
+                        connected_gears.push(number);
+                    }
+
+                }
+        }
+
+        assert!(!connected_gears.len() > 2);
+        if connected_gears.len() == 2 {
+            Some(connected_gears)
+        } else {
+            None
+        }
+    }
+
+
+    fn sum_gear_ratio(&self) -> u32 {
+        let mut sum = 0;
+
+        for gear in &self.potential_gears {
+            if let Some(gear_numbers) = self.is_gear(&gear) {
+                let ratio = gear_numbers[0].value * gear_numbers[1].value;  // safe because is_gear will alway return Option<Vec<Number>> with 2 Numbers!
+                sum += ratio;
+            }
+        }
+
+        sum
+    }
+
+
 }
 
 #[cfg(test)]
@@ -131,6 +213,11 @@ mod tests {
 
         let map = Map::new(input);
 
+        // part 1
         assert_eq!(4361, map.part_sum());
+
+        // part 2
+        assert_eq!(3, map.potential_gears.len());
+        assert_eq!(467835, map.sum_gear_ratio());
     }
 }
